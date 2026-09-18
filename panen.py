4eaed48816c35e915c6cb52e4f401d18e25f3e54
ZEFFANYA BYANZAHRA AMAREZ <zeffanya@gmail.com>
2026-09-18 10:49:28 +0000

    feat: Tambah fungsi perhitungan diskon harga panen

--- panen.py
+++ panen.py
@@ -2,3 +2,8 @@ def hitung_total_panen(list_panen):
     return sum(list_panen)
 
 print('Total Panen:', hitung_total_panen([100, 150, 200]), 'kg')
+
+def hitung_diskon(total_harga, diskon_persen):
+    return total_harga * (1 - diskon_persen / 100)
+
+print('Harga setelah diskon: Rp', hitung_diskon(1000000, 10))
